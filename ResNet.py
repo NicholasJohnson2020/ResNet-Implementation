94971ba93f192ade1bc9af2a83fec642bdd0c52a
Nicholas Johnson <35481278+NicholasJohnson2020@users.noreply.github.com>
2018-06-13 16:11:16 -0400

    Update ResNet.py

--- ResNet.py
+++ ResNet.py
@@ -1,2 +1,15 @@
 import torch
 import torch.nn as nn
+import torch.utils.model_zoo as model_zoo
+
+def conv3x3(in_planes, out_planes, stride = 1):
+  # 3x3 convultion with padding (no dimension reduction)
+  return nn.Conv2d(in_planes, out_planes, kernel_size = 3, stride = stride,
+                   padding = 1, bias = False)
+
+class BasicBlock(nn.Module):
+  expansion = 1
+  
+  def __init__(self, inplanes, planes, stride = 1, downsample = None):
+    super(BasicBlock, self).__init__()
+    self.
