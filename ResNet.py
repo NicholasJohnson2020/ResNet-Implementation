fa000be22ffd60a13a66b8041115fc57b849a8b5
Nicholas Johnson <35481278+NicholasJohnson2020@users.noreply.github.com>
2018-06-25 01:09:02 -0400

    Update ResNet.py

--- ResNet.py
+++ ResNet.py
@@ -39,18 +39,18 @@ class BasicBlock(nn.Module):
         return out
 
 class Bottleneck(nn.Module):
-    expansion = 2
+    bottle = 8
   
     def __init__(self, inplanes, planes, stride = 1, downsample = None):
         super(Bottleneck, self).__init__()
-        self.conv1 = nn.Conv2d(inplanes, planes, kernel_size = 1, bias = False)
-        self.bn1 = nn.BatchNorm2d(planes)
-        self.conv2 = nn.Conv2d(planes, planes, kernel_size = 3, stride = stride,
+        self.conv1 = nn.Conv2d(inplanes, bottle, kernel_size = 1, bias = False)
+        self.bn1 = nn.BatchNorm2d(bottle)
+        self.conv2 = nn.Conv2d(bottle, bottle, kernel_size = 3, stride = stride,
                                padding = 1, bias = False)
-        self.bn2 = nn.BatchNorm2d(planes)
-        self.conv3 = nn.Conv2d(planes, planes * self.expansion, kernel_size = 1,
+        self.bn2 = nn.BatchNorm2d(bottle)
+        self.conv3 = nn.Conv2d(bottle, planes, kernel_size = 1,
                                bias = False)
-        self.bn3 = nn.BatchNorm2d(planes * self.expansion)
+        self.bn3 = nn.BatchNorm2d(planes)
         self.relu = nn.ReLU(inplace = True)
         self.downsample = downsample
         self.stride = stride
