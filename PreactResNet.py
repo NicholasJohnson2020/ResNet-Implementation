bbf5de10fb5e0f452c8fb07039d4136e8b304c43
Nicholas Johnson <35481278+NicholasJohnson2020@users.noreply.github.com>
2018-06-24 23:38:41 -0400

    Update PreactResNet.py

--- PreactResNet.py
+++ PreactResNet.py
@@ -45,6 +45,7 @@ class ResNet(nn.Module):
         super(ResNet, self).__init__()
         self.conv1 = nn.Conv2d(3, 16, kernel_size = 3, stride = 1,
                                padding = 1, bias = False)
+        self.bnInitial = nn.BatchNorm2d(16)
         self.bnFinal = nn.BatchNorm2d(64)
         self.relu = nn.ReLU(inplace = True)
         self.layer1 = self._make_layer(block, 16, layers[0])
@@ -79,6 +80,8 @@ class ResNet(nn.Module):
     
     def forward(self, x):
         x = self.conv1(x)
+        x = self.bnInitial(x)
+        x = self.relu(x)
     
         x = self.layer1(x)
         x = self.layer2(x)
