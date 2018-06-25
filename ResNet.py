86208d2e23bb0731947e0ecae8b97efc968b71d7
Nicholas Johnson <35481278+NicholasJohnson2020@users.noreply.github.com>
2018-06-25 01:16:04 -0400

    Update ResNet.py

--- ResNet.py
+++ ResNet.py
@@ -45,7 +45,7 @@ class Bottleneck(nn.Module):
     def __init__(self, inplanes, planes, stride = 1, downsample = None):
         super(Bottleneck, self).__init__()
         self.conv1 = nn.Conv2d(inplanes, self.bottle, kernel_size = 1, bias = False)
-        self.bn1 = nn.BatchNorm2d(bottle)
+        self.bn1 = nn.BatchNorm2d(self.bottle)
         self.conv2 = nn.Conv2d(self.bottle, self.bottle, kernel_size = 3, stride = stride,
                                padding = 1, bias = False)
         self.bn2 = nn.BatchNorm2d(self.bottle)
